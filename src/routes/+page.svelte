230d0930e00d696b17e78c23b4c67d5f7861c3c0
ramankc6 <ramankc699@gmail.com>
2023-04-05 21:45:16 -0400

    redesign + resumepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,6 @@ import { onMount } from "svelte";
 import { fade } from 'svelte/transition';
 import { fly } from 'svelte/transition';
 
-
 //-------Variable declarations-------//
 
 //Index Declarations
@@ -21,7 +20,6 @@ let scaleIndex = 1;
 let text = "";
 let cursorColor = "#aeafad";
 
-
 //Const Text Declaration
 const fullText = ["Hey Snap!", "My name is Raman Khatri Chhetri"];
 const lessThan = '<';
@@ -34,6 +32,7 @@ let skillsTrans;
 let contactTrans;
 let projectsTrans;
 let userWidth;
+let audio;
 
 //Scrolling Declaration
 let y = 0;
@@ -50,14 +49,18 @@ let skills = true
 let contact = true
 let projects = true
 let showScroller = false
-
+let resumePage = false
 //-------Start onMount-------//
 
 //When the user loads the website it calls cusor Annimation which starts the typing annimation
 onMount(async () => {
+    alert('This website is still under construction, please excuse the mess')
     cursorAnnimation();
     imageResizer();
     updateOptionsDiv();
+    drake();
+    y = 0;
+
 });
 
 //-------Functions-------//
@@ -147,6 +150,7 @@ function imageResizer() {
 function buttonPressed (buttonType) {
     if (buttonType == 'resume') {
         contact = false;
+        resumePage = true;
         projects = false;
         skills = false;
         resumeTrans.style.transform = `scale(${scaleIndex * 3})`;
@@ -180,12 +184,18 @@ function buttonPressed (buttonType) {
     }
 }
 
+function drake() {
+    audio.play();
+    setTimeout(drake, 100)
+}
+
 function updateOptionsDiv() {
     if (y <= 1400) {
         resume = true;
         projects = true;
         skills = true;
         contact = true;
+        resumePage = false;
 
     }
     setTimeout(updateOptionsDiv, 0)
@@ -197,6 +207,7 @@ function updateOptionsDiv() {
 <style>
 
 @import url('https://fonts.cdnfonts.com/css/cascadia-code');
+@import url('https://fonts.googleapis.com/css2?family=Palanquin+Dark&display=swap');
 
 body{
     background-color: #1e1e1e;
@@ -270,11 +281,14 @@ body{
     height: 100%;
     overflow-x: hidden;
     overflow-y: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
 .laptopImg {
-    width: 100%;
-    height: 100%;
+    width: 85%;
+    height: 85%;
 }
 
 .aboutContainer {
@@ -300,14 +314,13 @@ body{
 .optionsDiv {
     display: flex;
     flex-direction: row;
-    width: 100%;
-    height: 100%;
+    width: 95%;
+    height: 95%;
     position: fixed;
-    top: 0;
-    left: 0;
+
     align-content: center;
-    overflow-x: hidden;
-    overflow-y: hidden;
+    /* overflow-x: hidden;
+    overflow-y: hidden; */
 }
 
 .leftOptionsDiv,
@@ -316,6 +329,11 @@ body{
     flex-direction: column;
     width:100%;
     height: 100%;
+
+}
+
+.leftOptionsDiv {
+    margin-right: 5%;
 }
 
 .resumeDiv,
@@ -324,25 +342,41 @@ body{
 .contactDiv {
     height: 100%;
     width: 100%;
-    align-items: center;
+
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
+    justify-content: center;
     transition: transform 1.5s;
-    /* border: 0; */
+    margin: 2%;
+    border-radius: 4%;
+    background-size: 110%;
+    background-position: 4% 4%;
+    border: 0;
 }
+
+.contactDiv,
+.skillsDiv {
+    justify-content: left;
+    align-items: left;
+}
+
+.resumeDiv,
+.projectsDiv {
+    align-items: center;
+}
+
 .resumeDiv:hover,
 .projectsDiv:hover,
 .skillsDiv:hover,
 .contactDiv:hover {
     transition: 0.5s;
-    height: 120%;
-    width: 140%;
+    height: 105%;
+    width: 105%;
 }
 .titleText {
-    color:white;
-    font-size: 45px;
-    margin-top: 10%;
+    font-size: 40px;
+    margin: 0%;
+    font-family: 'Palanquin Dark', sans-serif;
 }
 
 .optionsIcon {
@@ -372,10 +406,78 @@ button{
     width: 8w;
 
 }
+
+.optionsPage {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+
+}
+
+.resumeTitle {
+    color: white;
+    background-color: #1e1e1e;
+    border-radius: 9%;
+    min-width: 30%;
+    text-align: center;
+    color:white;
+    font-size: 40px;
+    margin: 0%;
+    font-family: 'Palanquin Dark', sans-serif;
+}
+.projectsTitle {
+    color: white;
+    min-width: 30%;
+    text-align: center;
+    color:white;
+    font-size: 40px;
+    margin: 0%;
+    font-family: 'Palanquin Dark', sans-serif;
+}
+.contactTitle,
+.skillsTitle{
+    color: black;
+    min-width: 30%;
+    text-align: center;
+    font-size: 40px;
+    margin: 0%;
+    font-family: 'Palanquin Dark', sans-serif;
+}
+
+/* audio{
+    opacity: 0;
+} */
+
+.rightOptionsDiv { direction: rtl; }
+.rightOptionsDiv > * { direction: ltr; }
+
+.resumePageContainer {
+    z-index: 7;
+    display: flex;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+}
+
+.resumePageDiv {
+    width: 80%;
+    height: 100%;
+}
+
+.resumePdf {
+    width: 100%;
+    height: 100%;
+}
 </style>
 
 <body>
-    <audio autoplay><source src='/music.mp3' type='audio/mpeg'></audio>
+    <!-- <audio controls autoplay bind:this={audio}>
+        <source src='/music.mp3' type='audio/mp3'>
+    </audio> -->
     {#if y > 50 && y < 1300}
         <div class= 'aboutBackground' in:fade><img bind:this="{laptopSize}" src = {laptopImg} alt = 'laptop' class = 'laptopImg' style= 'transform: {1 + (y - 50) / 1000}'></div>
     {:else if y < 50}
@@ -387,12 +489,12 @@ button{
                 <div class= 'typingDiv'>
                     <p class= "introText" >
                         <span class="inequalitySymbols">{lessThan}</span><span class='letterP'>
-                        p</span><span class="inequalitySymbols">
+                        h1</span><span class="inequalitySymbols">
                         {greaterThan}</span><span class=typingText>
                         {text}</span><span id="cursor" style="color: {cursorColor};">
                         |</span><span class="inequalitySymbols">
                         {lessThan}/</span><span class='letterP'>
-                        p</span><span class="inequalitySymbols">
+                        h1</span><span class="inequalitySymbols">
                         {greaterThan}</span>
                     </p>
                     {#if showScroller == true}
@@ -408,7 +510,15 @@ button{
         <div class='aboutPage'>
             <div class='aboutContainer' in:fade>
                 <div>
-                    <p>About</p>
+                    <p>
+                        <span class="inequalitySymbols">{lessThan}</span><span class='letterP'>
+                            h2</span><span class="inequalitySymbols">
+                            {greaterThan}</span><span class=typingText>
+                            About</span><span class="inequalitySymbols">
+                            {lessThan}/</span><span class='letterP'>
+                            h2</span><span class="inequalitySymbols">
+                            {greaterThan}</span>
+                    </p>
                 </div>
                 <div class='aboutTextDiv'>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi voluptas, quo repellendus doloremque fuga reprehenderit incidunt quaerat saepe laboriosam, expedita tempora neque ad quisquam quam perferendis vel autem magnam hic?</p>
@@ -417,43 +527,60 @@ button{
         </div>
     {/if}
     {#if y > 1400}
-        <div class='optionsDiv'>
-            <div class='leftOptionsDiv'>
-                {#if resume == true}
-                    <button bind:this="{resumeTrans}" on:click = {() => buttonPressed('resume')} class='resumeDiv' style='background-color: red' transition:fly={{ x: -100, y: -100, duration: 1000 }}>
-                    {#if projects == true}
-                        <p class='titleText'>Resume</p>
-                        <img src='/resumeIcon.png' alt='resumeIcon' class='optionsIcon'>
-                    {/if}
-                    </button>
-                {/if}
-                {#if projects == true}
-                    <button bind:this="{projectsTrans}" on:click = {() => buttonPressed('projects')} class='projectsDiv' style='background-color: green;' transition:fly={{ x: -100, y: 100, duration: 1000 }}>
-                    {#if resume == true}
-                        <p class='titleText'>Projects</p>
-                        <img src='/projectsIcon.png' alt='projectsIcon' class='optionsIcon'>
-                    {/if}
-                    </button>
-                {/if}
-            </div>
-
-            <div class='rightOptionsDiv'>
-                {#if skills == true}
-                    <button bind:this="{skillsTrans}" on:click = {() => buttonPressed('skills')} class='skillsDiv' style='background-color: yellow;' transition:fly={{ x: 100, y: -100, duration: 1000 }}>
+        <div class ='optionsPage'>
+            <div class='optionsDiv'>
+                <div class='leftOptionsDiv'>
+                    {#if resume == true && resumePage == false}
+                        <button bind:this="{resumeTrans}" on:click = {() => buttonPressed('resume')} class='resumeDiv' style='background-image: url("/resumeBackground.png");' in:fly={{ x: -100, y: -100, duration: 1000 }} out:fade>
                         {#if projects == true}
-                            <p class='titleText'>Skills</p>
-                            <img src='/skillsIcon.png' alt='skillsIcon' class='optionsIcon'>
+                            <div class='resumeTitle'>
+                            <p class='titleText'>Resume</p>
+                            </div>
+                            <!-- <img src='/resumeIcon.png' alt='resumeIcon' class='optionsIcon'> -->
                         {/if}
-                    </button>
-                {/if}
-                {#if contact == true}
-                    <button bind:this="{contactTrans}" on:click = {() => buttonPressed('contact')} class='contactDiv' style='background-color: blue;' transition:fly={{ x: 100, y: 100, duration: 1000 }}>
-                    {#if projects == true}
-                        <p class='titleText'>Contact</p>
-                        <img src='/contactIcon.png' alt='contactIcon' class='optionsIcon'>
+                        </button>
                     {/if}
-                    </button>
-                {/if}
+                    {#if resumePage == true}
+                    <div transition:fade class='resumePageContainer'>
+                        <div class= 'resumePageDiv'>
+                            <iframe class = 'resumePdf' title='' src="/Resume.pdf#toolbar=0" width="100%" height="500px">
+                            </iframe>
+                        </div>
+                    </div>
+                    {/if}
+                    {#if projects == true}
+                        <button bind:this="{projectsTrans}" on:click = {() => buttonPressed('projects')} class='projectsDiv' style='background-image: url("/projectBackground.png");' transition:fly={{ x: -100, y: 100, duration: 1000 }}>
+                        {#if resume == true}
+                            <div class='projectsTitle'>
+                            <p class='titleText'>Projects</p>
+                            <!-- <img src='/projectsIcon.png' alt='projectsIcon' class='optionsIcon'> -->
+                            </div>
+                        {/if}
+                        </button>
+                    {/if}
+                </div>
+
+                <div class='rightOptionsDiv'>
+                    {#if skills == true}
+                        <button bind:this="{skillsTrans}" on:click = {() => buttonPressed('skills')} class='skillsDiv' style='background-image: url("/skillsBackground.png");' transition:fly={{ x: 100, y: -100, duration: 1000 }}>
+                            {#if projects == true}
+                            <div class='skillsTitle'>
+                                <p class='titleText'>Skills</p>
+                                <!-- <img src='/skillsIcon.png' alt='skillsIcon' class='optionsIcon'> -->
+                            </div>
+                            {/if}
+                        </button>
+                    {/if}
+                    {#if contact == true}
+                        <button bind:this="{contactTrans}" on:click = {() => buttonPressed('contact')} class='contactDiv' style='background-image: url("/contactBackground.png"); background-position: center;' transition:fly={{ x: 100, y: 100, duration: 1000 }}>
+                        {#if projects == true}
+                        <div class='contactTitle'>
+                            <p class='titleText'>Contact</p>
+                        </div>
+                        {/if}
+                        </button>
+                    {/if}
+                </div>
             </div>
         </div>
     {/if}
